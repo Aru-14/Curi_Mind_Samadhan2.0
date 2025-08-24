b92678739e29856f3bdd4abaa66d6e17d08a3c49
Arunima Paunikar <augarunima1405@gmail.com>
2025-08-24 19:09:42 +0530

    Added task

---
+++ Tasks/PrintStudentDetails.js
@@ -0,0 +1,15 @@
+
+
+const studentObj = {
+  name: "Arun Jadhav",
+  age: 15,
+  class: 10,
+  rollNo:1
+};
+
+// Print details
+console.log("Student Details:");
+console.log("Name: " + studentObj.name);
+console.log("Age: " + studentObj.age);
+console.log("Grade: " + studentObj.class);
+console.log("Course: " + studentObj.rollNo);
