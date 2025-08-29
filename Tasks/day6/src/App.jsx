aafd17bc6de154d2f4ca93de571a5056fa3638db
gauri-create <gauribelokar2005@gmail.com>
2025-08-29 14:06:48 +0530

    Added Day6 task

---
+++ Tasks/day6/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+   
+    <div className=" mt-10 max-w-sm mx-auto bg-blue-200 rounded-2xl shadow-2xl overflow-hidden hover:shadow-xl transition-shadow duration-300">
+     
+      <div className="p-5 text-center">
+        <h2 className="text-xl font-semibold text-gray-800">Curi_Mind</h2>
+        <p className="text-sm text-purple-600 italic">We are a team of developers</p>
+        <p className="mt-3 text-gray-600 text-sm">Building AI powered Study Buddy for JEE</p>
+        
+      </div>
+    </div>
+  
+
+    </>
+  )
+}
+
+export default App
